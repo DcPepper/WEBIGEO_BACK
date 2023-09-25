51f678c69a8d6965eb79f8b8715ccfaeab79bf9a
goli-sateesh-6011 <goli.sateesh@gmail.com>
2023-09-25 14:04:50 +0000

    Satish

--- backend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from backend.models import Country, Record, Quiz
-
-admin.site.register(Country)
-admin.site.register(Record)
-admin.site.register(Quiz)
-
-# Register your models here.
